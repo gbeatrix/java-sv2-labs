7834dd00e72d5250fe26e84aa8ff4c12ae0da6d9
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-18 12:19:08 +0200

    statements/Kifejezések

---
+++ training-solutions/src/main/java/statements/StatementMain.java
@@ -0,0 +1,13 @@
+package statements;
+
+public class StatementMain {
+    public static void main(String[] args) {
+        int x = 5+6;
+        int y = 11-x;
+        int z = 8;
+        boolean b = x>y;
+        boolean c = b || z>5;
+        System.out.println(""+x+y+z+b+c);
+        System.out.println(++z);
+    }
+}

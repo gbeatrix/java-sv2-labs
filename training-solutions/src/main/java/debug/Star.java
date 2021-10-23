ba584d9c106ffe3f5ac6c4d0cae5c153f3989add
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-23 11:57:29 +0200

    debug/Csillagok javítása

--- training-solutions/src/main/java/debug/Star.java
+++ training-solutions/src/main/java/debug/Star.java
@@ -2,8 +2,8 @@ package debug;
 
 public class Star {
     public void writeStars(int[] numbers) {
+        int prev = 0;
         for (int i = 0; i < numbers.length; i++) {
-            int prev = 0;
             int stars = numbers[i] - prev;
             printStars(stars);
             prev = numbers[i];

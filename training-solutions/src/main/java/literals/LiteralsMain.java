108ff95c742fa41ed44df16b8dd601bbb22b48ed
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-26 14:21:21 +0200

    literals/Osztás

--- training-solutions/src/main/java/literals/LiteralsMain.java
+++ training-solutions/src/main/java/literals/LiteralsMain.java
@@ -4,5 +4,15 @@ public class LiteralsMain {
     public static void main(String[] args) {
         System.out.println("" + 1 + 2);
         System.out.println(Integer.toString(1) + 2);
+
+        double quotient = 3/4; // 3/4 maradékos osztás, eredménye 0, majd ez automatikus konverzióval lesz 0.0 lebegőpontos érték
+        System.out.println(quotient);
+        // legalább az egyik tényezőt lebegőpontos szám literálként megadva már a várt 0.75 eredményt kapjuk
+        quotient = 3/4d;
+        System.out.println(quotient);
+        quotient = 3./4;
+        System.out.println(quotient);
+        quotient = 3e0/4;
+        System.out.println(quotient);
     }
 }

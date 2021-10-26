c7f862ded05820f296948645655a7f188cae7c58
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-26 14:07:33 +0200

    literals/Összefűzés

---
+++ training-solutions/src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,8 @@
+package literals;
+
+public class LiteralsMain {
+    public static void main(String[] args) {
+        System.out.println("" + 1 + 2);
+        System.out.println(Integer.toString(1) + 2);
+    }
+}

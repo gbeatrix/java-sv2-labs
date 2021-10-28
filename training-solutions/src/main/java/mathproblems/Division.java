e4ef6d2a3d08a6bd521a6dc556a6429ad222a686
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-28 17:27:07 +0200

    mathproblems/Számok indexe

--- training-solutions/src/main/java/mathproblems/Division.java
+++ training-solutions/src/main/java/mathproblems/Division.java
@@ -9,8 +9,17 @@ public class Division {
         }
     }
 
+    public void getRightNumbers(int[] numbers){
+        for (int i = 1; i < numbers.length; i++) {
+            if(numbers[i]%i==0){
+                System.out.print(numbers[i] + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Division division = new Division();
         division.getDivisors(12);
+        division.getRightNumbers(new int[]{2, 5, 23, 56, -36, 45, 78, 14, -8, 4, 10});
     }
 }

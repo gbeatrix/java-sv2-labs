59fe559268118baf75331b292942b498a96815e0
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-28 18:16:48 +0200

    typeconversion/Double-ből double

---
+++ training-solutions/src/main/java/typeconversion/Conversion.java
@@ -0,0 +1,8 @@
+package typeconversion;
+
+public class Conversion {
+    public double convertDoubleToDouble(double x){
+        int y = (int)x;
+        return y;
+    }
+}
---
+++ training-solutions/src/main/java/typeconversion/ConversionMain.java
@@ -0,0 +1,8 @@
+package typeconversion;
+
+public class ConversionMain {
+    public static void main(String[] args) {
+        Conversion conversion = new Conversion();
+        System.out.println(conversion.convertDoubleToDouble(5.342));
+    }
+}

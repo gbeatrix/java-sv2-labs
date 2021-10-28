15c6ea557df1e6015950bb5d3b64a08acfccc2f1
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-28 18:23:46 +0200

    typeconversion/Egész számokból egész számok

--- training-solutions/src/main/java/typeconversion/Conversion.java
+++ training-solutions/src/main/java/typeconversion/Conversion.java
@@ -5,4 +5,16 @@ public class Conversion {
         int y = (int)x;
         return y;
     }
+
+    public byte[] convertIntArrayToByteArray(int[] arr){
+        byte[] result = new byte[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>=0 && arr[i]<128){
+                result[i] = (byte)arr[i];
+            } else {
+                result[i] = (byte)-1;
+            }
+        }
+        return result;
+    }
 }
--- training-solutions/src/main/java/typeconversion/ConversionMain.java
+++ training-solutions/src/main/java/typeconversion/ConversionMain.java
@@ -1,8 +1,11 @@
 package typeconversion;
 
+import java.util.Arrays;
+
 public class ConversionMain {
     public static void main(String[] args) {
         Conversion conversion = new Conversion();
         System.out.println(conversion.convertDoubleToDouble(5.342));
+        System.out.println(Arrays.toString(conversion.convertIntArrayToByteArray(new int[]{5, -14, 6, 2, 125, 354, 9738, 3})));
     }
 }

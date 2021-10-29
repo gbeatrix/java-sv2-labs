7a0efd5ac6fbce277ad4a3cca1f60bee649bcc68
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-29 13:28:38 +0200

    stringconcat/Alkalmazott adatai szövegesen

---
+++ training-solutions/src/main/java/stringconcat/employee/Employee.java
@@ -0,0 +1,18 @@
+package stringconcat.employee;
+
+public class Employee {
+    private String name;
+    private String job;
+    private int salary;
+
+    public Employee(String name, String job, int salary) {
+        this.name = name;
+        this.job = job;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + job + " - " + salary + " Ft";
+    }
+}
---
+++ training-solutions/src/main/java/stringconcat/employee/EmployeeMain.java
@@ -0,0 +1,8 @@
+package stringconcat.employee;
+
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Kis Géza", "minőségellenőr", 520000);
+        System.out.println(employee);
+    }
+}

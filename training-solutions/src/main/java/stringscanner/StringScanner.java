8144a726f4a6d60705a1178d2378ca9166f9c29e
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-31 17:52:47 +0100

    stringscanner/Szöveg beolvasása és kiírása

---
+++ training-solutions/src/main/java/stringscanner/StringScanner.java
@@ -0,0 +1,32 @@
+package stringscanner;
+
+import java.util.Scanner;
+
+public class StringScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Írj egy összetett mondatot: ");
+        String sentence = scanner.nextLine();
+        Scanner stringScanner = new Scanner(sentence);
+
+        for(int i=0; i<1; i++) {
+            if(stringScanner.hasNext()){
+                System.out.println(stringScanner.next());
+                i--;
+            }
+        }
+        System.out.println();
+
+        System.out.print("Írj egy összetett mondatot: ");
+        sentence = scanner.nextLine();
+        stringScanner = new Scanner(sentence).useDelimiter(", ");
+        for(int i=0; i<1; i++) {
+            if(stringScanner.hasNext()){
+                System.out.print(stringScanner.next());
+                System.out.println(stringScanner.hasNext()?", ":"");
+                i--;
+            }
+        }
+    }
+}

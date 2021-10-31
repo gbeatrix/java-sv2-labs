d759e18936a23fb674ca2d391b8d2b52facf853c
gbeatrix <gatibeatrix+github@gmail.com>
2021-10-31 17:59:44 +0100

    stringscanner/Számsor konvertálása

---
+++ training-solutions/src/main/java/stringscanner/IntScanner.java
@@ -0,0 +1,39 @@
+package stringscanner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IntScanner {
+    public String convertInts(String ints){
+        Scanner intScanner = new Scanner(ints).useDelimiter(";");
+        List<Integer> bigInts = new ArrayList<>();
+        for (int i = 0; i < 1; i++) {
+            if(intScanner.hasNextInt()){
+                int k = intScanner.nextInt();
+                if(k>100){
+                    bigInts.add(k);
+                }
+                i--;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean first = true;
+        for(int bigInt : bigInts){
+            if(first){
+                first = false;
+            } else {
+                sb.append(",");
+            }
+            sb.append(bigInt);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        IntScanner intScanner = new IntScanner();
+        System.out.println(intScanner.convertInts("5;3;107;12;123;18;198"));
+    }
+}

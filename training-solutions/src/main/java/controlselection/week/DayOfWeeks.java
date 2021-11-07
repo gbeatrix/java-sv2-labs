940f5713f4b0c32abcad9851c23bbc2cf72167d9
gbeatrix <gatibeatrix+github@gmail.com>
2021-11-07 12:40:51 +0100

    controlselection/Hét napjai

---
+++ training-solutions/src/main/java/controlselection/week/DayOfWeeks.java
@@ -0,0 +1,18 @@
+package controlselection.week;
+
+public class DayOfWeeks {
+    public String whichDay(String day){
+        switch (day.toLowerCase()){
+            case "hétfő":
+                return "hét eleje";
+            case "kedd", "szerda", "csütörtök":
+                return "hét közepe";
+            case "péntek":
+                return "majdnem hétvége";
+            case "szombat", "vasárnap":
+                return "hét vége";
+            default:
+                return "ismeretlen nap";
+        }
+    }
+}

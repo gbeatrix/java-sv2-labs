6e6e1c4e486080d92258416eee596a5a9030d205
gbeatrix <gatibeatrix+github@gmail.com>
2021-11-27 17:01:38 +0100

    algorithmsdecision/Nagy összegű bankszámlák

---
+++ training-solutions/src/main/java/algorithmsdecision/bankaccounts/BankAccount.java
@@ -0,0 +1,41 @@
+package algorithmsdecision.bankaccounts;
+
+public class BankAccount {
+    private String nameOfOwner;
+    private String accountNumber;
+    private int balance;
+
+    public BankAccount(String nameOfOwner, String accountNumber, int balance) {
+        this.nameOfOwner = nameOfOwner;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getNameOfOwner() {
+        return nameOfOwner;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public boolean withdraw(int amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deposit(int amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ training-solutions/src/main/java/algorithmsdecision/bankaccounts/BankAccountDecisionMaker.java
@@ -0,0 +1,14 @@
+package algorithmsdecision.bankaccounts;
+
+import java.util.List;
+
+public class BankAccountDecisionMaker {
+    public boolean containsBalanceGreaterThan(List<BankAccount> accounts, int min) {
+        for(BankAccount account : accounts){
+            if(account.getBalance()>min){
+                return true;
+            }
+        }
+        return false;
+    }
+}

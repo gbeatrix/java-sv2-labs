780cbc20bf4f9ee0e35ea6a17794dfa1e2b57a83
gbeatrix <gatibeatrix+github@gmail.com>
2021-11-27 21:37:50 +0100

    immutable/Autó

---
+++ training-solutions/src/main/java/immutable/Car.java
@@ -0,0 +1,33 @@
+package immutable;
+
+import java.time.LocalDate;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private final int yearOfProduction;
+
+    public Car(String brand, String model, int yearOfProduction) {
+        if (brand == null || brand.isEmpty()) {
+            throw new IllegalArgumentException("Brand name cannot be empty!");
+        }
+        if (yearOfProduction > LocalDate.now().getYear()) {
+            throw new IllegalArgumentException("Year of production cannot be in the future!");
+        }
+        this.brand = brand;
+        this.model = model;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+}

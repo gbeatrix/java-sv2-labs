7046181c341db571697f502f59c36f3ac948fbcc
gbeatrix <gatibeatrix+github@gmail.com>
2021-11-28 20:51:24 +0100

    constructoroverloading/Bicikli apróhirdetés

---
+++ training-solutions/src/main/java/constructoroverloading/advertisement/BikeAdvertisement.java
@@ -0,0 +1,51 @@
+package constructoroverloading.advertisement;
+
+import java.util.List;
+
+public class BikeAdvertisement {
+    private String description;
+    private int price;
+    private List<String> extras;
+    private String brand;
+    private String serialNumber;
+
+    public BikeAdvertisement(String description, int price) {
+        this.description = description;
+        this.price = price;
+    }
+
+    public BikeAdvertisement(String description, int price, String brand) {
+        this(description, price);
+        this.brand = brand;
+    }
+
+    public BikeAdvertisement(String description, int price, String brand, List<String> extras) {
+        this(description, price, brand);
+        this.extras = extras;
+    }
+
+    public BikeAdvertisement(String description, int price, String brand, List<String> extras, String serialNumber) {
+        this(description, price, brand, extras);
+        this.serialNumber = serialNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public List<String> getExtras() {
+        return extras;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+}

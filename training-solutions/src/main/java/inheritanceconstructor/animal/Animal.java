e8d5aa2682326f2d422894f9cb46f680033a647c
gbeatrix <gatibeatrix+github@gmail.com>
2021-11-29 14:07:59 +0100

    inheritanceconstructor/Konstruktorok hívási lánca

---
+++ training-solutions/src/main/java/inheritanceconstructor/animal/Animal.java
@@ -0,0 +1,8 @@
+package inheritanceconstructor.animal;
+
+public class Animal {
+    public Animal() {
+        super();
+        System.out.println("Animal class constructor");
+    }
+}
---
+++ training-solutions/src/main/java/inheritanceconstructor/animal/AnimalMain.java
@@ -0,0 +1,12 @@
+package inheritanceconstructor.animal;
+
+public class AnimalMain {
+    public static void main(String[] args) {
+        new Sparrow();
+        /*
+        Animal class constructor
+        Bird class constructor
+        Sparrow class constructor
+        */
+    }
+}

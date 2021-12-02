a63bb771d0d7b17540e995989aa53035d83a0a0d
gbeatrix <gatibeatrix+github@gmail.com>
2021-12-02 12:56:36 +0100

    objectclass/A táska tartalma teszteset bemásolása

---
+++ training-solutions/src/test/java/objectclass/SimpleBagTest.java
@@ -0,0 +1,60 @@
+package objectclass;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleBagTest {
+
+    @Test
+    void testCreate() {
+        //Given
+        SimpleBag bag = new SimpleBag();
+        //Then
+        assertTrue(bag.isEmpty());
+        assertEquals(0, bag.size());
+    }
+
+
+    @Test
+    void testPut() {
+        //Given
+        SimpleBag bag = new SimpleBag();
+        //When
+        bag.putItem("alma");
+        bag.putItem(12);
+        bag.putItem(new Book("Rejtő Jenő", "Az ellopott cirkáló"));
+        bag.putItem(new Beer("Borsodi", 239));
+        //Then
+        assertFalse(bag.isEmpty());
+        assertEquals(3, bag.size());
+
+    }
+
+    @Test
+    void testIteration() {
+        //Given
+        SimpleBag bag = new SimpleBag();
+        //When
+        bag.putItem(new Beer("Heineken", 250));
+        bag.putItem(new Book("Rejtő Jenő", "Az ellopott cirkáló"));
+        bag.putItem(new Beer("Borsodi", 239));
+        while (bag.hasNext()) {
+        }
+        //Then
+        assertEquals(2, bag.getCursor());
+    }
+
+    @Test
+    void testContains() {
+        //Given
+        SimpleBag bag = new SimpleBag();
+        //When
+        bag.putItem(new Beer("Borsodi", 239));
+        bag.putItem(new Book("Rejtő Jenő", "Az ellopott cirkáló"));
+        //Then
+        assertTrue(bag.contains(new Beer("Borsodi", 239)));
+        assertTrue(bag.contains(new Book("Rejtő Jenő", "Az ellopott cirkáló")));
+        assertFalse(bag.contains(new Book("Rejtő Jenő", "A megkerült cirkáló")));
+    }
+}

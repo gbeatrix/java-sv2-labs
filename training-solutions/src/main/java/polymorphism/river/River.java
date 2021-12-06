fd3027b13413c239d0767326db23b0db6698eb5c
gbeatrix <gatibeatrix+github@gmail.com>
2021-12-06 20:52:21 +0100

    polymorphism/Folyó

---
+++ training-solutions/src/main/java/polymorphism/river/River.java
@@ -0,0 +1,19 @@
+package polymorphism.river;
+
+public abstract class River implements Water {
+    private String name;
+    private int length;
+
+    public River(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}

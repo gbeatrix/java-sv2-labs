092fda5cd6cea6fb6f505b297578003ee30a29fe
gbeatrix <gatibeatrix+github@gmail.com>
2022-01-13 13:06:08 +0100

    collectionslist/Lejárati dátum

---
+++ training-solutions/src/main/java/collectionslist/FoodStore.java
@@ -0,0 +1,23 @@
+package collectionslist;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FoodStore {
+    private List<Food> foods = new LinkedList<>();
+
+    public void addFood(Food food) {
+        foods.add(food);
+    }
+
+    public void sellFirst(Food food) {
+        if (food.getExpirationDate().equals(LocalDate.now())) {
+            foods.add(0, food);
+        }
+    }
+
+    public List<Food> getFoods() {
+        return List.copyOf(foods);
+    }
+}

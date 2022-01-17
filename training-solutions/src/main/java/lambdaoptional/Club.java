43dc03a71405c890160474fa383f348a1676fcd1
gbeatrix <gatibeatrix+github@gmail.com>
2022-01-17 09:58:36 +0100

    lambdaoptional/Klubtagok

---
+++ training-solutions/src/main/java/lambdaoptional/Club.java
@@ -0,0 +1,33 @@
+package lambdaoptional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class Club {
+    private List<Member> members;
+
+    public Club(List<Member> members) {
+        this.members = members;
+    }
+
+    public Optional<Member> findFirst(Predicate<Member> predicate) {
+        for (Member member : members) {
+            if (predicate.test(member)) {
+                return Optional.of(member);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Double> averageNumberOfSkills() {
+        if (members == null || members.isEmpty()) {
+            return Optional.empty();
+        }
+        double sum = 0;
+        for (Member member : members) {
+            sum += member.getSkills().size();
+        }
+        return Optional.of(sum / members.size());
+    }
+}
---
+++ training-solutions/src/main/java/lambdaoptional/Gender.java
@@ -0,0 +1,5 @@
+package lambdaoptional;
+
+public enum Gender {
+    MALE, FEMALE
+}

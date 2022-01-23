2da26a719171f88862da4e012d866a0648050194
gbeatrix <gatibeatrix+github@gmail.com>
2022-01-23 10:29:47 +0100

    lambda/Osztási maradékok

---
+++ training-solutions/src/main/java/lambda/Remainder.java
@@ -0,0 +1,10 @@
+package lambda;
+
+import java.util.List;
+
+public class Remainder {
+    public List<Integer> changeNumbersToRemainders(List<Integer> numbers, int divisor) {
+        numbers.replaceAll(number -> number % divisor);
+        return numbers;
+    }
+}
